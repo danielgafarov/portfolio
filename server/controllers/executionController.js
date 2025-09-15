b270a6b702748daa7867066a34aadd5c9b93f8f5
danielgafarov <gafarovdaniel16@gmail.com>
2025-09-15 19:31:15 +0200

    adjusted execution paths and parameters, added index.js to .gitignore

--- server/controllers/executionController.js
+++ server/controllers/executionController.js
@@ -9,9 +9,10 @@ const getExec = async (req, res) => {
   for (const x in req.query.params.values)
     values.push(req.query.params.values[x])
   const id = req.query.params.id
-  const python = spawn(entryPoints[id].command, [`./controllers/programs/${id}/${entryPoints[id].file}`,values]);
+  const child = spawn(entryPoints[id].command, [entryPoints[id].file,values]);
+  console.log(child)
   let data = "";
-  for await (const chunk of python.stdout) {
+  for await (const chunk of child.stdout) {
     data += chunk;
   }
   res.send({ data });
